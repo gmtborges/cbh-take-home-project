071465f683463f35063c004ed1d4313a727e32e0
Gustavo Martins T. Borges <gustavo.mtborges@gmail.com>
2023-02-12 12:35:19 -0300

    refactor the if/elses

--- dpk.js
+++ dpk.js
@@ -6,24 +6,26 @@ const MAX_PARTITION_KEY_LENGTH = 256;
 exports.deterministicPartitionKey = (event) => {
   let candidate;
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
+  if (!event) {
+    return "0";
   }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
+  if (event.partitionKey) {
+    candidate = event.partitionKey;
+  } else {
+    const data = JSON.stringify(event);
+    candidate = crypto.createHash("sha3-512").update(data).digest("hex");
+  }
+
+  if (candidate && typeof candidate !== "string") {
+    candidate = JSON.stringify(candidate);
   } else {
     candidate = TRIVIAL_PARTITION_KEY;
   }
+
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
     candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
   }
+
   return candidate;
 };
--- dpk.test.js
+++ dpk.test.js
@@ -6,8 +6,20 @@ describe("deterministicPartitionKey", () => {
     expect(trivialKey).toBe("0");
   });
 
-  it("Returns the hash when passes the partitionKey", () => {
+  it("Returns the partitionKey when passes the partitionKey less then 256", () => {
     const trivialKey = deterministicPartitionKey({ partitionKey: 300 });
-    expect(trivialKey).toBe("12123123");
+    expect(trivialKey).toBe("300");
+  });
+
+  it("Returns the literal '0' when passes a wrong partitionKey", () => {
+    const trivialKey = deterministicPartitionKey({ wrongPartitionKey: 300 });
+    expect(trivialKey).toBe("0");
+  });
+
+  it("Returns the literal '0' when passes a wrong partitionKey", () => {
+    const trivialKey = deterministicPartitionKey({
+      partitionKey: 256 * 1_000_000_000_000_000_000,
+    });
+    expect(trivialKey).toBe("0");
   });
 });
